78cc1054a15fb4eac78da00d87ca6266fc48bb49
unknown <Administrator@lulei.lbn.com>
2018-12-29 17:13:23 +0800

    公共父类

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -1,18 +1,19 @@
 <?php
 	namespace app\index\controller;
 
-	use \think\Controller;
-	//公共继承的基类
+	use think\Controller;
+	//公共继承的基类,主要用于检测用户是否登陆
 	class Base extends Controller 
 	{
 		//构造方法用于验证session是否存在
 		public function __construct()
 		{
-			echo "此处检验session";
+			//不添加会导致继承问题
+			parent::__construct();
+			if(empty(session("User"))){
+				//登陆验证
+				$this->redirect("/index.php/index/login/index");
+			}
 		}
 
-		public function index()
-		{
-			
-		}
 	}
