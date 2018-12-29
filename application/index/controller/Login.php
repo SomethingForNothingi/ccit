f431ddd694d7f948bc1b962922575d32e59a13a8
unknown <Administrator@lulei.lbn.com>
2018-12-29 17:15:44 +0800

    登录控制器

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -1,15 +1,21 @@
 <?php
 	namespace app\index\controller;
-	use \think\Controller;
+	use app\index\controller\Common;
 	use \think\captcha\Captcha;
 	use \think\Db;
-	class Login extends Controller
+	class Login extends Common
 	{
 		//渲染页面
 		public function index()
 		{
-			$captcha = new \think\captcha\Captcha();
 			return $this->fetch();
+		}
+
+		//退出
+		public function logout()
+		{
+			session("User",null);
+			return $this->fetch('Login/index');
 		}
 
 		//注册账号
@@ -32,27 +38,24 @@
 		//注册检测
 		public function checkReg()
 		{
-			$icon = "";
-			//头像检测
-			if(count($_FILES)){
-				$file = request()->file("icon");
-				$info = $file->validate(['ext'=>'jpg,png,gif'])->move("../uploads");
-				//图片的完整路径
-				if($info){
-					$icon = "uploads\\".$info->getSaveName();
-				} else {
-					$this->error($file->getError());
-				}
-		    }
-
-		    //密码判断
-		    
+			$file = request()->file("icon");
+			$info = $file->move("./uploads");
+			$icon = "/uploads\\".$info->getSaveName();
 		    $data = input("post.");
 		    $data['icon'] = $icon;
 		    $data['pwd'] = md5($data['pwd1']);
 		    unset($data['pwd1']);
 		    unset($data['pwd2']);
-		    dump($data);
+		    //账号是否存在于数据库
+		    $have = Db::table("user")->where("act=".$data['act'])->count();
+		    if($have == 0){
+		    	//不存在添加
+			    Db::table("user")->insert($data);
+			    $this->redirect("login/index");
+		    } else {
+		    	//存在退回注册
+		    	$this->redirect("login/registe");
+		    }
 		}
 
 		//获取账号数据
@@ -60,38 +63,40 @@
 		{
 			$act = trim(input("get.act"))??0;
 			$isReg = Db::table("user")->where("act=".$act)->find();
-
 			//存在账号
-			if(is_null($isReg))
-			{
+			if(is_null($isReg)){
 				if(strlen($act) < 10) {
 					exit(json_encode(array("code"=>0,"msg"=>"学号输入错误")));
+				} else{
+					//注册成功，待添加新功能
+					exit(json_encode(array("code"=>1,"msg"=>"允许注册")));
 				}
-				exit(json_encode(array("code"=>1,"msg"=>"允许注册")));
-			} else {
-				exit(json_encode(array("code"=>0,"msg"=>"账号已存在")));
+			} else if($act == 0){
+				exit(json_encode(array("code"=>0,"msg"=>"请输入学号")));
+			}else{
+				exit(json_encode(array("code"=>0,"msg"=>"学号已存在")));
 			}
 		}
 
 		//登陆检测
 		public function check()
 		{
-			$username = trim(input('post.act'));
-			$pwd = trim(input('post.pwd'));
+			//$captcha = new Captcha();
+			$act = input('post.act');
+			$pwd = md5(input('post.pwd'));
 			$verifycode = trim(input('post.verify'));
-
-			if($username == ''){
-				exit(json_encode(array('code'=>1,'msg'=>'用户名不能为空')));
-			}
-			if($pwd == ''){
-				exit(json_encode(array('code'=>1,'msg'=>'密码不能为空')));
-			}
-			if($verifycode == ''){
-				exit(json_encode(array('code'=>1,'msg'=>'请输入验证码')));
-			}
-			// 验证验证码
-			if(!captcha_check($verifycode)){
-				exit(json_encode(array('code'=>1,'msg'=>'验证码错误')));
+			//检测账号密码
+			$ck = $this->checkAp($act,$pwd);
+			if(Db::table("user")->where("act=".$act)->count()==0){
+				exit(json_encode(array("code"=>0,"msg"=>"该账号尚未注册")));
+			}else if($ck != 1){
+				exit(json_encode(array("code"=>0,"msg"=>"密码输入错误")));
+			} else if(!captcha_check($verifycode)){		//检测验证码
+				exit(json_encode(array("code"=>0,"msg"=>"验证码错误")));
+			}else {
+				//设置session
+				session("User",$this->getUserMes($act));
+				exit(json_encode(array("code"=>1,"msg"=>"登陆成功")));
 			}
 		}
 	}
